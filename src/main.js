fce74f0b0464a6587428cbb58176c79550cb6755
Yingpeng Wang <yp.wangt@gmail.com>
2017-11-29 14:58:09 -0600

    imported axios and vueAxios

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
