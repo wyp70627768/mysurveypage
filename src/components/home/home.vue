e76c90a852fe107a6986f8d9327cdcead2c78456
Yingpeng Wang <yp.wangt@gmail.com>
2017-11-29 15:13:45 -0600

    create the survey page and the general frame for this project

---
+++ src/components/home/home.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="suvery-list-content">
+        <table class="surveylist-table">
+            <thead>
+                <th v-for="column in columns" :class="column.field">{{column.label}}</th>
+            </thead>
+            <tbody>
+                <tr v-for="survey in surveys">
+                    <td v-for="column in columns" :class="column.field" class="survey-body-column">
+                        <a v-if="column.field === 'name'" :href="'#/surveys/' + survey._id">{{survey.title}}</a>
+                        <a v-if="column.field === 'result'" :href="'#/submissions/' + survey._id">Survey Result</a>                          
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <input type="button" class="createSurvery-btn" name="Create Survey" value="Create Survey" @click="createSurvey()">
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data: function() {
+            return{
+                surveys: [],
+                columns: [
+                    {
+                      label: 'Survey Name',
+                      field: 'name'
+                    },
+                    {
+                      label: 'View Suvey Result',
+                      field: 'result'
+                    }
+                ]
+            }
+        },
+
+        created: function() {
+            this.getSurveys();
+        },
+
+        methods: {
+            getSurveys() {
+                let uri = '/api/surveys';
+                this.axios.get(uri).then((response) => {
+                    this.surveys = response.data;
+                }).catch(e => {
+                    console.log(e);
+                });
+            },
+            createSurvey () {
+              this.$router.push("/createsurvey");
+            }
+        }
+    }
+</script>
+
+<style scoped lang='scss'>
+@import "../../scss/home"
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/home/home.vue'
+import ViewPage from '@/components/viewPage/viewPage.vue'
+import CreateSurvey from '@/components/createSurvey/createsurvey.vue'
+import SubmissionViewPage from '@/components/submissions/viewPage.vue' 
+import SummaryPage from '@/components/summaryPage/summary.vue' 
 
 Vue.use(Router)
 
@@ -8,8 +13,32 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/surveys',
+      component: Home,
+      },
+    {
+      path: '/surveys/:id',
+      component: ViewPage,
+      props: { default: true, sidebar: false }
+      },
+      {
+      path: '/createsurvey',
+      name: 'CreatePage',
+      component: CreateSurvey
+      },
+      {
+      path: '/submissions/:id',
+      name: 'Submission',
+      component: SubmissionViewPage
+      },
+      {
+        path: '/summary',
+        name: 'SummaryPage',
+        component: SummaryPage
+      }
   ]
 })
